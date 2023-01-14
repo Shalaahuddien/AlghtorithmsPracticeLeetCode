4829e6729de7f0f56463d5f7681ed962d667933e
Osman yousif <103122098+OsmanX5@users.noreply.github.com>
2023-01-14 07:01:54 +0200

    Time: 99 ms (65.77%), Space: 38.8 MB (13.51%) - LeetHub

--- 2367-number-of-arithmetic-triplets/2367-number-of-arithmetic-triplets.cs
+++ 2367-number-of-arithmetic-triplets/2367-number-of-arithmetic-triplets.cs
@@ -1,7 +1,7 @@
 public class Solution {
     public int ArithmeticTriplets(int[] nums, int diff) {
         int n = nums.Length;
-        List<int> numbers = nums.ToList();
+        HashSet<int> numbers = new HashSet<int>(nums);
         int res=0;
         for(int i=1;i<n-1;i++){
            if(numbers.Contains(nums[i]+diff) && numbers.Contains(nums[i]-diff)){
