e09cd4c992e20a3cd58c15b7cdeff83ded613106
Osman yousif <103122098+OsmanX5@users.noreply.github.com>
2023-01-25 06:43:56 +0200

    Time: 158 ms, Memory: 45 MB - LeetHub

--- search-for-a-range/search-for-a-range.cs
+++ search-for-a-range/search-for-a-range.cs
@@ -1,21 +1,41 @@
 public class Solution {
     public int[] SearchRange(int[] nums, int target) {
+        int HigherIndex(int l ,int r ){
+            if (l+1>=r){
+                if(nums[r] == target) return r;
+                if(nums[l] == target) return l;
+                return -1;
+            }
+            int mid = l + (r-l)/2;
+            if(nums[mid] == target&& nums[mid+1]>target&&nums[mid-1]<=target){
+                return mid;
+            }
+            if (nums[mid]<=target) return HigherIndex(mid,r);
+            return HigherIndex(l,mid);
+    }
+        int LowerIndex(int l ,int r ){
+            int mid = l + (r-l)/2;
+           // Console.WriteLine($"l = {l} r  = {r} , mid ={mid}");
+            if (l+1>=r){
+               // Console.WriteLine("Break l = {l} r = {r}");
+                if(nums[l] == target) return l;
+                if(nums[r] == target) return r;
+                return -1;
+            }
+            if(nums[mid] == target && nums[mid+1]>=target&&nums[mid-1]<target){
+           //     Console.WriteLine($"Founded in l = {l} r  = {r} , mid ={mid} {nums[mid]}, {nums[mid+1]},{nums[mid-1]}");
+                return mid;
+            }
+            if (nums[mid]<target) return LowerIndex(mid,r);
+            return LowerIndex(l,mid);
+    }
+
         int n = nums.Length;
+        if (n ==0) return new int[2]{-1,-1};
         int l =0;
         int r = n-1;
-        while(l<=r){
-            int mid = l + (r-l)/2;
-            if(nums[mid] == target){
-                int[] res = new int[2];
-                res[0] = mid;
-                res[1] = mid;
-                for(int j =mid+1;j<n;j++) if(nums[j] ==target)res[1] =j; else j=n;
-                for(int j =mid-1;j>=0;j--)if(nums[j] ==target)res[0] =j; else j=-1;
-                return res;
-            }
-            if (nums[mid]<target)  l = mid+1;
-            else r = mid-1;
-        }
-        return new int[2]{-1,-1};
+        //Console.WriteLine(LowerIndex(l,r));
+        return new int[2]{LowerIndex(l,r),HigherIndex(l,r)};
     }
+
 }
