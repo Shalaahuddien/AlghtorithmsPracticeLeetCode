887b485ab09506721a529283cf3753d0516453dc
Osman yousif <103122098+OsmanX5@users.noreply.github.com>
2023-01-26 06:40:18 +0200

    Time: 216 ms (84.77%), Space: 51.7 MB (12.18%) - LeetHub

--- 0852-peak-index-in-a-mountain-array/0852-peak-index-in-a-mountain-array.cs
+++ 0852-peak-index-in-a-mountain-array/0852-peak-index-in-a-mountain-array.cs
@@ -5,14 +5,14 @@ public class Solution {
         int r= n-1;
         while(l+1<r){
             int mid = l+(r-l)/2;
-            if(arr[mid]>arr[mid-1]&&arr[mid]>arr[mid+1]){
-                return mid;
-            }
-            else if(arr[mid]>arr[mid-1]) 
+            if(arr[mid]>=arr[mid-1]) 
                 l =mid;
             else
                 r = mid;
         }
-        return arr[(l+r)/2];
+        int m = l+(r-l)/2;
+        if (arr[m]>arr[l] && arr[m]>arr[r]) return m;
+        else if (arr[r]>arr[l]) return r;
+        return l;
     }
 }
