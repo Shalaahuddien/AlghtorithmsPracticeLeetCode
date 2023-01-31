4ebf98cc491ee07c8a048de0104392fa978f2351
Osman yousif <103122098+OsmanX5@users.noreply.github.com>
2023-01-31 20:45:49 +0200

    Time: 33 ms (86.96%), Space: 13.8 MB (54.95%) - LeetHub

---
+++ 0202-happy-number/0202-happy-number.py
@@ -0,0 +1,17 @@
+class Solution:
+    def isHappy(self, n: int) -> bool:
+        def convert(x):
+            res= 0
+            while x>0:
+                digit = x%10;
+                x=x//10
+                res += digit*digit
+            return res
+        oldNumbers =[]
+        nowNumber = convert(n)
+        while(nowNumber!=1):
+            if ( nowNumber in oldNumbers):
+                return False
+            oldNumbers.append(nowNumber)
+            nowNumber = convert(nowNumber)
+        return True;
